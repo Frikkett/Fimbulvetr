818598b4e9992fb407625a4d6735d9487d05fdb4
Frikkett <gilberte@stu.otc.school.nz>
2020-07-28 13:30:01 +1200

    3nd commit test

--- Fimbulvetr.py
+++ Fimbulvetr.py
@@ -1,3 +1,30 @@
 print("Hvat segirthu?")
 print("Cause I'm doin fine")
 print("Although I will say the Fimbulvetr is quite annoying, what with all the ashfall")
+
+#Function Definitions
+
+def Menu():
+    while True:
+        print('''
+         __________________________________________
+        |                                          |
+        | |||\ /|||  H]]]]]  |///\  |/|  Hlll  Hl| |
+        | |||\V/|||  H]|__   |/|\/\ |/|  Hlll  Hl| |
+        | |||   |||  H]]]]]  |/| \/\|/|  Hlll  Hl| |
+        | |||   |||  H]|__   |/|  \///|  HlllL/ll| |
+        | |||   |||  H]]]]]  |/|   \//|   \lllll/  |
+        |__________________________________________|''')
+        menuChoice = int(input(''' Please select ONE of the following options:
+         ______
+        |  /\  | Load Game. This will automatically save your game and load a
+        | /==\ | different game instead. To select this option, please type "A"  
+        |/____\| and press the [Enter] key.
+         ______
+        | | \  |
+        | |-<  |
+        |_|__|_|
+        '''))
+
+
+#MainGame Loop
