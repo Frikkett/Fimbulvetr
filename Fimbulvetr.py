62ce9c90561dd2d0f09a40b805e7b82e29266712
Frikkett <gilberte@stu.otc.school.nz>
2020-07-29 10:53:56 +1200

    Working on Menu Function

--- Fimbulvetr.py
+++ Fimbulvetr.py
@@ -2,9 +2,16 @@ print("Hvat segirthu?")
 print("Cause I'm doin fine")
 print("Although I will say the Fimbulvetr is quite annoying, what with all the ashfall")
 
-#Function Definitions
+# Function Definitions
+
+ListA = list(["A","a",'"a"','"A"'," A"," a","A ","a ",])
+ListB = list(["B","b",'"b"','"B"'," B"," b","B ","b ",])
+ListC = list(["C","c",'"c"','"C"'," C"," c","C ","c ",])
+ListD = list(["D","d",'"d"','"D"'," D"," d","D ","d ",])
+
 
 def Menu():
+    global ListA
     while True:
         print('''
          __________________________________________
@@ -15,16 +22,31 @@ def Menu():
         | |||   |||  H]|__   |/|  \///|  HlllL/ll| |
         | |||   |||  H]]]]]  |/|   \//|   \lllll/  |
         |__________________________________________|''')
-        menuChoice = int(input(''' Please select ONE of the following options:
+        print(''' Please select ONE of the following options:
          ______
         |  /\  | Load Game. This will automatically save your game and load a
-        | /==\ | different game instead. To select this option, please type "A"  
-        |/____\| and press the [Enter] key.
+        | /==\ | different game instead. 
+        |/____\| To select this option, please type "A" and press the [Enter] key.
          ______
-        | | \  |
-        | |-<  |
-        |_|__|_|
-        '''))
+        | | \  | New Game. This will create a new game file, so you can play from
+        | |-<  | the beginning of the game.
+        |_|__|_| To select this option, please type "B" and press the [Enter] key.
+         ______
+        |  ,-- | Delete Game. This will Delete a previously created game. 
+        | |    | =====================>[WARNING]<=====================
+        |__'--_| THIS IS IRREVERSIBLE. TO SELECT THIS OPTION, TYPE "C" AND PRESS [Enter]
+        
+        ''')
+        while True:
+            menuChoice = str(input("===>>"))
+            if menuChoice in ListA:
+                print("hi")
+            elif menuChoice in ListB:
+                print("bye")
+            elif menuChoice in ListC:
+                print("die")
+            else:
+                print("Type an actual option please.")
 
 
 #MainGame Loop
